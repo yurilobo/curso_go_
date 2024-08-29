c595c442da7daf7e165807e285471ddc66fd69ef
Yuri <yuri.anderson.lobo@gamail.com>
2024-08-28 23:04:49 -0300

    interfaces

--- 10-struct/main.go
+++ 10-struct/main.go
@@ -8,6 +8,20 @@ type Endereco struct {
 	Cidade     string
 	Estado     string
 }
+
+// nas interfaces de go so e possivel setar metodos
+type Pessoa interface {
+	Desativar()
+}
+
+type Empresa struct {
+	Nome string
+}
+
+func (e Empresa) Desativar() {
+
+}
+
 type Cliente struct {
 	Nome  string
 	Idade int
@@ -17,15 +31,21 @@ type Cliente struct {
 
 func (c *Cliente) Desativar() {
 	c.Ativo = false
+	fmt.Printf("O cliente %s foi desativo", c.Nome)
 }
 
+func Desativacao(pessoa Pessoa) {
+	pessoa.Desativar()
+}
 func main() {
 	yuri := Cliente{
 		Nome:  "Yuri",
 		Idade: 29,
 		Ativo: true,
 	}
+	minhaEmpresa := Empresa{}
 
+	Desativacao(minhaEmpresa)
 	yuri.Endereco.Cidade = "Fortaleza"
 	yuri.Ativo = false
 	yuri.Desativar()
