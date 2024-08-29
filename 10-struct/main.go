54b3f5261041d8f280be2b0115d7f158a5c0e6b9
Yuri <yuri.anderson.lobo@gamail.com>
2024-08-28 22:49:25 -0300

    struct

--- 10-struct/main.go
+++ 10-struct/main.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
 type Cliente struct {
 	Nome  string
 	Idade int
 	Ativo bool
+	Endereco
+}
+
+func (c *Cliente) Desativar() {
+	c.Ativo = false
 }
 
 func main() {
@@ -14,5 +25,9 @@ func main() {
 		Idade: 29,
 		Ativo: true,
 	}
+
+	yuri.Endereco.Cidade = "Fortaleza"
+	yuri.Ativo = false
+	yuri.Desativar()
 	fmt.Printf("Nome: %s, Idade: %d, Ativo: %t", yuri.Nome, yuri.Idade, yuri.Ativo)
 }
