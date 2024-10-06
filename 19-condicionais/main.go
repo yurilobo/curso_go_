f38bc1f7d2c68ff31f3c8b4d838a36217c363067
Yuri <yuri.anderson.lobo@gamail.com>
2024-10-06 17:29:44 -0300

    condicionais

---
+++ 19-condicionais/main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+	a := 1 //312
+	// b := 2
+	// c := 3
+
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3:
+		println("c")
+	default:
+		println("iai")
+	}
+
+	// if a > b && c > a {
+	// 	println("a>b && c > a")
+	// }
+
+	// if a==b{
+	// 	println(a)
+	// }else{
+	// 	println(b)
+	// }
+
+}
