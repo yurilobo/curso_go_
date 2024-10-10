a75849c6a7a14967082ee67fb46d3d572038205a
Yuri <yuri.anderson.lobo@gamail.com>
2024-10-09 22:56:24 -0300

    usando json

---
+++ 23-json/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Conta struct {
+	Numero int
+	Saldo  int `json:"s"`
+}
+
+func main() {
+	conta := Conta{Numero: 1, Saldo: 100}
+	res, err := json.Marshal(conta)
+	if err != nil {
+		println(err)
+	}
+
+	println(string(res))
+
+	err = json.NewEncoder(os.Stdout).Encode(conta)
+	if err != nil {
+		println(err)
+	}
+
+	jsonPuro := []byte(`{"Numero":2, "s":200}`) //"Saldo":200}
+	var contaX Conta
+	err = json.Unmarshal(jsonPuro, &contaX)
+	if err != nil {
+		println(err)
+	}
+	println(contaX.Saldo)
+
+}
