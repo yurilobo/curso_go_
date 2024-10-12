758fdb61391544e090441fa7385c8dbbe7e5e7c5
yurilobo <40974897+yurilobo@users.noreply.github.com>
2024-10-11 23:10:29 -0300

    Revert "Revert "manipulando headers""

--- Busca-cep-projeto/main.go
+++ Busca-cep-projeto/main.go
@@ -7,5 +7,16 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 func BuscaCEP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello world!"))
 }
