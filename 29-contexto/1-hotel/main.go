d7d3008b4e97e194ddee5c4232219f523af9fd5b
Yuri <yuri.anderson.lobo@gamail.com>
2024-11-24 16:57:05 -0300

    uso de contexto

---
+++ 29-contexto/1-hotel/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	bookHotel(ctx)
+}
+func bookHotel(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("Hotel booking canceled, Timeout reached  ")
+		return
+	case <-time.After(1 * time.Second): //definomos o tempo do booking
+		fmt.Println("Hotel booked.")
+	}
+}
